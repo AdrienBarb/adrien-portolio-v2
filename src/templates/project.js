c99e5880d8257777a300cd7f621a84955785b140
Adrien Barbier <abarbier@debutant-e.pro>
2021-06-08 22:24:58 +0200

    (fix) - comment project template

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,39 +1,39 @@
-import React, { useEffect } from 'react'
-import {
-  StyledProject,
-  StyledProjectWrapper,
-  StyledProjectDescription,
-  StyledVideoWrapper,
-} from '../styles/projectStyles'
-import { Helmet } from 'react-helmet'
+// import React, { useEffect } from 'react'
+// import {
+//   StyledProject,
+//   StyledProjectWrapper,
+//   StyledProjectDescription,
+//   StyledVideoWrapper,
+// } from '../styles/projectStyles'
+// import { Helmet } from 'react-helmet'
 
-const Project = ({ pageContext }) => {
-  const { article } = pageContext
+// const Project = ({ pageContext }) => {
+//   const { article } = pageContext
 
-  return (
-    <div>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Adrien Barbier | {article.title}</title>
-      </Helmet>
-      <StyledProjectWrapper>
-        <StyledProject>
-          <StyledVideoWrapper>
-            <img src={article.projectVideo.fluid.src} alt="Adrien Project"/>
-          </StyledVideoWrapper>
-          <StyledProjectDescription>
-            <h1>{article.title}</h1>
-            <p>{article.description?.description}</p>
-            {article.website && (
-              <a href={article.website} target="_blank" rel="noreferrer">
-                Visiter le site
-              </a>
-            )}
-          </StyledProjectDescription>
-        </StyledProject>
-      </StyledProjectWrapper>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <Helmet>
+//         <meta charSet="utf-8" />
+//         <title>Adrien Barbier | {article.title}</title>
+//       </Helmet>
+//       <StyledProjectWrapper>
+//         <StyledProject>
+//           <StyledVideoWrapper>
+//             <img src={article.projectVideo.fluid.src} alt="Adrien Project"/>
+//           </StyledVideoWrapper>
+//           <StyledProjectDescription>
+//             <h1>{article.title}</h1>
+//             <p>{article.description?.description}</p>
+//             {article.website && (
+//               <a href={article.website} target="_blank" rel="noreferrer">
+//                 Visiter le site
+//               </a>
+//             )}
+//           </StyledProjectDescription>
+//         </StyledProject>
+//       </StyledProjectWrapper>
+//     </div>
+//   )
+// }
 
-export default Project
+// export default Project
